d34be559107af7648e32b95d7b2636b6d872be1e
PeterTodorovv <petertodorovv@gmail.com>
2021-12-18 21:34:42 +0200

    12/18

---
+++ 01.Recursion and Backtracking/02.RecursiveDrawing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.RecursiveDrawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Drawer(n);
+        }
+
+        public static void Drawer(int n)
+        {
+            if(n == 0)
+            {
+                return;
+            }
+            Console.WriteLine(new string('*', n));
+
+            Drawer(n - 1);
+
+            Console.WriteLine(new string('#', n));
+        }
+    }
+}
---
+++ 01.Recursion and Backtracking/05.PathsInLabyrinth/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.PathsInLabyrinth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            char[,] matrix = ReadMatrix(height, width);
+        }
+
+        public static void FindPaths(char[,] labirint)
+        {
+
+        }
+
+        private static char[,] ReadMatrix(int height, int width)
+        {
+            char[,] matrix = new char[height, width];
+            for(int i = 0; i < height; i++)
+            {
+                char[] line = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                for(int j = 0; j < width; j++)
+                {
+                    matrix[i, j] = line[j];
+                }
+            }
+            return matrix;
+        }
+    }
+}
